9432b397227f03d56c9412ed1e0e277f53d3135d
Felipe Souza <fsouzacamargo@gmail.com>
2019-02-21 22:27:30 -0300

    exercicios do começo do terceiro semestre da faculdade

---
+++ EMP/Calculador/Calculadora.js
@@ -0,0 +1,5 @@
+function pegarNume() {
+  var numero = parseFloat(document.getElementById("nume").value);
+  console.log(numero);
+  numero = "";
+}
---
+++ EMP/Readme.md
@@ -0,0 +1,1 @@
+Nessa pasta estão todos os exercícios que eu mesmo projetei para que, toda a matéria do 1º semestre fosse estudada!
